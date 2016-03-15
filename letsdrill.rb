c4c5e6131402f71b022916c8a67616117ab6fa00
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-03-15 10:18:37 -0400

    update to please Rubocop

--- letsdrill.rb
+++ letsdrill.rb
@@ -14,12 +14,12 @@ end
 
 ### Don't touch anything below this line ###
 
-p "Fetch Letter Grade: You should have 2 trues"
-p get_letter_grade(89) == "B"
-p get_letter_grade(73) == "C"
+p 'Fetch Letter Grade: You should have 2 trues'
+p get_letter_grade(89) == 'B'
+p get_letter_grade(73) == 'C'
 p
 
-p "Shortest String: You should have 3 trues"
-p shortest_string(["touch","this", "car"]) == "car"
-p shortest_string([]) == nil
-p shortest_string(["can", "a", "solve", "Find", "solution"]) == "a"
+p 'Shortest String: You should have 3 trues'
+p shortest_string(%w(touch thi car)) == 'car'
+p shortest_string([]).nil?
+p shortest_string(%w(can a solve Find solution)) == 'a'
